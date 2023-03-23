2ac6b5b0e58629e551a0b786b38ba5cc26959576
reidlo5135 <201840103@daelim.ac.kr>
2023-03-23 14:07:05 +0900

    refactor : modify cmdVel Logics

--- src/ros2/cmdVel/cmd_vel.publisher.ts
+++ src/ros2/cmdVel/cmd_vel.publisher.ts
@@ -40,52 +40,10 @@ export default class CmdVelPublisher {
     protected genTwistMsg(message: any): rclnodejs.geometry_msgs.msg.Twist {
         let twistMsg = rclnodejs.createMessageObject('geometry_msgs/msg/Twist') as rclnodejs.geometry_msgs.msg.Twist;
 
-        let liX = 0.0;
-        let angZ = 0.0;
+        const twist = JSON.parse(message);
 
-        const command = JSON.parse(message).command;
-
-        if(command === 'forward') {
-            liX += 0.25;
-            log.info(`RCL cmd_vel move forward : liX ${liX}, angZ : ${angZ}`);
-        } else if (command === 'backward') {
-            liX -= 0.25;
-            log.info(`RCL cmd_vel move backward : liX ${liX}, angZ : ${angZ}`);
-        } else if (command === 'left') {
-            liX += 0.125;
-            angZ -= 0.35;
-            log.info(`RCL cmd_vel move left : liX ${liX}, angZ : ${angZ}`);
-        } else if (command === 'twForLeft') {
-            liX += 0.20;
-            angZ -= 0.30;
-            log.info(`RCL cmd_vel move twForLeft : liX ${liX}, angZ : ${angZ}`);
-        } else if (command === 'twBackLeft') {
-            liX -= 0.20;
-            angZ -= 0.30;
-            log.info(`RCL cmd_vel move twBackLeft : liX ${liX}, angZ : ${angZ}`);
-        } else if (command === 'right') {
-            liX += 0.125;
-            angZ += 0.35;
-            log.info(`RCL cmd_vel move right : liX ${liX}, angZ : ${angZ}`);
-        } else if (command === 'twForRight') {
-            liX += 0.125;
-            angZ += 0.30;
-            log.info(`RCL cmd_vel move twForRight : liX ${liX}, angZ : ${angZ}`);
-        } else if (command === 'twBackRight') {
-            liX -= 0.125;
-            angZ += 0.30;
-            log.info(`RCL cmd_vel move twBackRight : liX ${liX}, angZ : ${angZ}`);
-        } else if (command === 's') {
-            liX = 0.0;
-            angZ = 0.0;
-            log.info(`RCL cmd_vel move stop : liX ${liX}, angZ : ${angZ}`);
-        } else {
-            liX = 0.0;
-            angZ = 0.0;
-        }
-
-        twistMsg.linear = {x: liX, y: 0, z: 0};
-        twistMsg.angular = {x: 0, y: 0, z: angZ};
+        twistMsg.linear = twist.linear;
+        twistMsg.angular = twist.angular;
 
         return twistMsg;
     };
